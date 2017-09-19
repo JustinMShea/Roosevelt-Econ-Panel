a3b9e34f1e91936abe3821f2f507c2b88b844dba
JustinMShea <jshea@mail.roosevelt.edu>
2017-09-19 07:58:56 -0500

    removed unused functions

--- R/functions.R
+++ R/functions.R
@@ -1,45 +1,3 @@
-# Hamilton Filter
-library(xts)
-HLfilter.lm <- function(x, h = 8) {
-        
-        DF <- merge(lag.xts(x, k = -h, na.pad = TRUE),
-                    x,
-                    lag.xts(x, k = 1, na.pad = TRUE),
-                    lag.xts(x, k = 2, na.pad = TRUE),
-                    lag.xts(x, k = 3, na.pad = TRUE))
-        
-        colnames(DF) <- c("x_h", "x", "x_1", "x_2", "x_3")
-        
-        HL_filter <- lm(x_h ~ x + x_1 + x_2 + x_3, data = DF)
-        
-        HL_filter
-        
-}
-
-HLfilter <- function(x, h = 8) {
-        
-        DF <- merge(x,
-                    lag.xts(x, k = h, na.pad = TRUE),
-                    lag.xts(x, k = h+1, na.pad = TRUE),
-                    lag.xts(x, k = h+2, na.pad = TRUE),
-                    lag.xts(x, k = h+3, na.pad = TRUE))
-        
-        colnames(DF) <- c("x_h", "x", "x_1", "x_2", "x_3")
-        
-        HL_filter <- lm(x_h ~ x + x_1 + x_2 + x_3, data = DF)
-        
-        
-        HL_fit <- as.xts(unname(HL_filter$fitted.values),
-                         order.by = as.yearqtr(names(HL_filter$fitted.values)))
-        
-        HL_resid <- as.xts(unname(HL_filter$residuals),
-                           order.by = as.yearqtr(names(HL_filter$residuals)))
-        
-        
-        merge(DF$x_h, DF$x_h-DF$x, HL_fit, HL_resid)
-        
-}
-
 
 #Fisher Real Index computation
 Fisher <- function(component, price, quantity, base  = 249) {
@@ -61,4 +19,31 @@ Fisher <- function(component, price, quantity, base  = 249) {
         # Calculate Real 
         Real <- merge(component/Fisher_index)
         return(Real)
+}
+
+# Hamilton Filter
+
+
+HLfilter <- function(x, h = 8) {
+        require(xts)
+        DF <- merge(x,
+                    lag.xts(x, k = h, na.pad = TRUE),
+                    lag.xts(x, k = h+1, na.pad = TRUE),
+                    lag.xts(x, k = h+2, na.pad = TRUE),
+                    lag.xts(x, k = h+3, na.pad = TRUE))
+        
+        colnames(DF) <- c("x_h", "x", "x_1", "x_2", "x_3")
+        
+        HL_filter <- lm(x_h ~ x + x_1 + x_2 + x_3, data = DF)
+        
+        
+        HL_fit <- as.xts(unname(HL_filter$fitted.values),
+                         order.by = as.yearqtr(names(HL_filter$fitted.values)))
+        
+        HL_resid <- as.xts(unname(HL_filter$residuals),
+                           order.by = as.yearqtr(names(HL_filter$residuals)))
+        
+        
+        merge(DF$x_h, DF$x_h-DF$x, HL_fit, HL_resid)
+        
 }
